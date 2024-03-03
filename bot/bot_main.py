e46b1e4231450d0308a76b46e909fd5af2072934
Rerium <rer.of.riverwood@gmai.com>
2024-03-04 00:00:38 +0300

    добавление пары тестовых команд

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -1,22 +1,41 @@
 import discord
+from discord.ext import commands
+
 from bot.bot_setting import *
 
-class arkBot(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+#   Инициализируем бота
+intents = discord.Intents.default()                                             # Подключаем "Разрешения"
+intents.message_content = True
 
-    async def on_message(self, message):
+arkBot = commands.Bot(command_prefix='?', intents=intents)                      # Задаём префикс и интенты
+
+# С помощью декоратора создаём первую команду
+@arkBot.command()
+async def ping(message):
+    await message.send('pong')
+
+
+@arkBot.event
+async def on_ready():
+        print(f'Logged on as {arkBot.user}, (ID: {arkBot.user.id})')
+
+
+@arkBot.event
+async def on_message(message):
         # don't respond to ourselves
-        if message.author == self.user:
+        if message.author == arkBot.user:
             return
-
-        if message.content == 'ping':
+        if 'ping' in message.content:
             await message.channel.send('pong')
 
 
+
+
+
+
+
+
+#малозначащая функция на текущий момент
 def botStart():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = arkBot(intents=intents)
-    client.run(settings['token'])
+    arkBot.run(settings['token'])
     pass    
