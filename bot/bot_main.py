e6480ed126f7d11ac1b102adcdb285dfbadfa8e7
Rerium <rer.of.riverwood@gmai.com>
2024-03-09 19:40:33 +0300

    Добавление пары новых команд

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands
-import math, random
+import random, os
 
 from bot.bot_setting import *
-from bot.phrases import *
+from bot.phrases import phrases, triggers, command
 
 #   Инициализируем бота
 intents = discord.Intents.default()                                                             # Подключаем "Разрешения"
@@ -14,22 +14,50 @@ arkBot = commands.Bot(command_prefix=settings['prefix'], intents=intents)       
 # С помощью декоратора создаём первую команду
 @arkBot.command()
 async def ping(message):
-    await message.send('pong') 
+    await message.send("pong")
+    pass
+
+@arkBot.command()
+async def list(message):
+    msg = ""
+    for cmd in command:
+        msg = msg + cmd+'\n'
+    await message.send(str(msg))
+    pass
+        
+@arkBot.command()
+async def info(message):
+    file = discord.File("bot/image/info.png", filename="info.png")
+
+    embed = discord.Embed(color= 0x87498f) 
+    embed.set_image(url="attachment://info.png")
+    embed.add_field(name = 'Информация о боте', value =str(phrases['info']), inline = False)   
+
+    await message.send(file=file, embed=embed)
+    pass
+
+
+                
+
 
 
 @arkBot.event
 async def on_ready():
-        print(f'Logged on as {arkBot.user}, (ID: {arkBot.user.id})')
-
+    print(f'Logged on as {arkBot.user}, (ID: {arkBot.user.id})')
 
 @arkBot.event
+async def on_guild_join(test):
+    await print(test)
+     
+
+""" @arkBot.event
 async def on_message(message):
-        if message.author == arkBot.user:                                                       # не отвечать самому себе
-            return
+    if message.author == arkBot.user:                                                       # не отвечать самому себе
+        return
         
-        if 'донат' in message.content:                                                          # Рандомное срабатывание на слово(для веселья)
-            if (round(random.gammavariate(1, 0.5)) == 1):
-                await message.channel.send(phrases['donate'])
+    if 'донат' in message.content:                                                          # Рандомное срабатывание на слово(для веселья)
+        if (round(random.gammavariate(1, 0.5)) == 1):
+            await message.channel.send(phrases['donate']) """
 
 #малозначащая функция на текущий момент
 def botStart():
--- bot/phrases.py
+++ bot/phrases.py
@@ -1,4 +1,10 @@
 phrases = {
     "donate": "Самый выгодный донат на Tulen Store! https://tulen.store/ref/415",
+    "info": "Бот бы написан за пару бутылок пиваса и несколько дней гугления. При написании применялась нейросеть gigacode для автодополнения. \n Репозиторий бота - https://gitverse.ru/a.felt/DiscordChanBot \n Сервер для какого делался бот - https://discord.gg/hnbPS9rKQa \n ТГ канал разработчика - https://t.me/arkaniums \n Стримы - https://www.twitch.tv/rerium \n Поддержать - https://www.donationalerts.com/r/rerium"
 }
 triggers = ['donate',]
+
+command =[" ```?ping``` Команда для теста отзыва бота, в дальнейшем возвращает ping до 8.8.8.8 от сервера",
+           "```?list``` Команда со списком основных команд",
+           "```?info``` Команда с информацией о боте",]
+
