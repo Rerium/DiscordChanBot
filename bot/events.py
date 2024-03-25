91cb423104cbd2ce6e682c53d2088033a9314c05
a.felt <a.felt@yandex.ru>
2024-03-26 00:57:01 +0300

    just for fun

--- bot/events.py
+++ bot/events.py
@@ -1,8 +1,25 @@
 from bot.bot_main import arkBot
 from bot.phrases import phrases, command
+import disnake, random
 
 # Реакция на загрузку бота
 @arkBot.event
 async def on_ready():
     print(f'Logged on as {arkBot.user} (ID: {arkBot.user.id})')                                 # Выводит имя подключенного бота и его ID
 
+@arkBot.event
+async def on_message(message):
+    if message.author == arkBot.user:                                                       # не отвечать самому себе
+                await arkBot.process_commands(message)
+                return
+
+    if 'донат' in message.content:                                                          # Рандомное срабатывание на слово(для веселья)
+        if (round(random.gammavariate(1, 0.5)) == 1):
+            embed = disnake.Embed(color= 0x87498f) 
+            embed.set_image(url="https://tulen.store/storage/thumbs/1920x360_fit/LYCw088gGUthyWU0VoslL6W6iWi0rNLgL7Ldlf2A.jpg")
+            embed.add_field(name = 'Лучший донат?', value =str(phrases['donate']), inline = False)   
+
+            await message.channel.send(embed=embed)
+            
+    await arkBot.process_commands(message)
+    pass
--- bot/phrases.py
+++ bot/phrases.py
@@ -1,5 +1,5 @@
 phrases = {
-    "donate": "Самый выгодный донат на Tulen Store! https://tulen.store/ref/415",
+    "donate": f"Самый выгодный донат на Tulen Store! \n https://tulen.store/ref/415",
     "info": "Бот бы написан за пару бутылок пиваса и несколько дней гугления. При написании применялась нейросеть gigacode для автодополнения. \n Репозиторий бота - https://gitverse.ru/a.felt/DiscordChanBot \n Сервер для какого делался бот - https://discord.gg/hnbPS9rKQa \n ТГ канал разработчика - https://t.me/arkaniums \n Стримы - https://www.twitch.tv/rerium \n Поддержать - https://www.donationalerts.com/r/rerium"
 }
 triggers = ['donate',]
